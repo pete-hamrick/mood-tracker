f12aaa4ecab6bd262fd4f77e6ecbe05d01f4a267
Pete Hamrick <phamrick@cvent.com>
2022-11-10 15:06:19 -0800

    installed session and wrote register.html and basic register logic in app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -24,18 +29,31 @@ def login():
             # render the login.html
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return "future home of registering a user"
+    # return "future home of registering a user"
     # TODO make register.html
+    session.clear()
+    
     # TODO write behavior for:
-        # POST method
-            # handle if the user doesn't input email or password or password confirmation
-            # connect to the database
-            # check if the email is already registered
-                # render the register.html template but with an error message?
-            # hash the password
-            # insert the email and password hash into the database
-            # redirect to login so the user can...login
-        # GET method
-            # render the register.html
+    # POST method
+    if request.method == "POST":
+        userEmail = request.form.get("email")
+        userPassword = request.form.get("password")
+        passwordConfirmation = request.form.get("confirm")
+
+        # handle if the user doesn't input email or password or password confirmation
+        if not userEmail or not userPassword or not passwordConfirmation:
+            message = "Please enter a valid email and password and confirm your password"
+            return render_template("register.html", message=message)
+        # connect to the database
+        # check if the email is already registered
+            # render the register.html template but with an error message?
+        # hash the password
+        # insert the email and password hash into the database
+        # redirect to login so the user can...login
+
+    # GET method
+    else:
+        # render the register.html
+        return render_template("register.html")
